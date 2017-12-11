6dd1b2c9cbfe89194e378ff9593f4016b86b9336
Alexandr Burtovoy <volgobur@gmail.com>
2017-12-11 18:31:35 +0300

    Rm passwords

--- routes/index.ts
+++ routes/index.ts
@@ -4,8 +4,6 @@ import { web3 } from '../helpers/web'
 
 const FAUCET_ACCOUNT = process.env.FAUCET_ACCOUNT
 if (!FAUCET_ACCOUNT) { throw new Error('Please, set FAUCET_ACCOUNT env variable') }
-const FAUCET_PASSWORD = process.env.FAUCET_PASSWORD
-if (!FAUCET_PASSWORD) { throw new Error('Please, set FAUCET_PASSWORD env variable') }
 
 const GIFT_AMOUNT = web3.toWei(0.1, 'ether')
 
@@ -21,7 +19,6 @@ router.get('/faucet', (req: express.Request, res: express.Response, next: expres
 
 router.post('/faucet/request', function(req: express.Request, res: express.Response, next: express.NextFunction) {
   let callFaucet = (address: string, callback: (err: Error, value: string) => void) => {
-    if(web3.personal) web3.personal.unlockAccount(FAUCET_ACCOUNT, FAUCET_PASSWORD, 10000000)
     web3.eth.sendTransaction({from: FAUCET_ACCOUNT, to: address, value: GIFT_AMOUNT}, callback)
   }
   let address = req.body.address
--- webpack.config.js
+++ webpack.config.js
@@ -5,20 +5,13 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
 require('dotenv').config({ path: '.env' });
 
-var srcPath  = path.join(__dirname, '/public/javascripts/'),
-    distPath = path.join(__dirname, '/public/javascripts/');
+var srcPath  = path.join(__dirname, '/assets/javascripts/')
+var distPath = path.join(__dirname, '/public/javascripts/')
 
 const RECEIVER = process.env.RECEIVER;
 if (!RECEIVER) throw new Error('Please, set receiver address to RECEIVER env variable')
-
-const GATEWAY_URL = process.env.GATEWAY_URL;
-if (!GATEWAY_URL) throw new Error('Please, set gateway url to GATEWAY_URL env variable')
-
 const VYNOS_URL = process.env.VYNOS_URL;
 if (!VYNOS_URL) throw new Error('Please, set url to Vynos to VYNOS_URL env variable')
-
-const ETHEREUM_API = process.env.ETHEREUM_API;
-if (!ETHEREUM_API) throw new Error('Please, set ETHEREUM_API env variable')
 
 module.exports = {
     context: srcPath,
