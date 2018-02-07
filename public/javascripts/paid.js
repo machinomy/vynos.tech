31ec6f34a26ab9a8f63e82500cd0dd7afc09d9a0
xjenek <xxjenekxx@gmail.com>
2018-02-07 17:00:49 +0300

    fixed init and check error

--- public/javascripts/paid.js
+++ public/javascripts/paid.js
@@ -13,7 +13,7 @@ let loadContent = (token) => {
 		window.paywallPrice = response.getResponseHeader('paywall-price')
 		window.paywallAddress = response.getResponseHeader('paywall-address')
 
-	  if (!window.paywallPrice) {
+		if (!window.paywallPrice) {
 			$('#buy').hide()
 			$('.b-article__for').hide()
 		}
@@ -29,8 +29,8 @@ if (displayButton) {
 }
 
 function canBeHandled(message) {
-	message.indexOf('doesn\'t have enough funds to send') !== -1 ||
-		message.indexOf('insufficient funds') !== -1
+	return message.indexOf('doesn\'t have enough funds to send') !== -1 ||
+	message.indexOf('insufficient funds') !== -1
 }
 
 let buyButton = document.getElementById('buy')
@@ -40,26 +40,25 @@ if (buyButton) {
 		vynos.ready().then(wallet => {
 			let web3 = new Web3(wallet.provider)
 			web3.eth.getAccounts(function (err, accounts) {
-				if (!accounts || !accounts.length) return vynos.display();
-				let account = accounts[0]
-				if (!account) return vynos.display();
-				let title = 'Outline'
-				let receiver = window.paywallAddress
-				let amount = window.paywallPrice
-				let gateway = window.paywallGateway
-				let meta = window.paywallMeta
+				if (!accounts || !accounts.length) vynos.display();
+				wallet.initAccount().then(() => {
+					let receiver = window.paywallAddress
+					let amount = window.paywallPrice
+					let gateway = window.paywallGateway
+					let meta = window.paywallMeta
 
-				wallet.buy(receiver, amount, gateway, meta).then(result => {
-					if (!result) return Promise.reject('vynos locked');
-					let contentKey = $('meta[property="og:url"]').attr('content')
-					localStorage[contentKey] = result.token
-					loadContent(result.token)
-					console.log('Result: ', result)
-					buyButton.style.display = 'none'
-				}).catch(err => {
-					console.error(err)
-					if (err.message && canBeHandled(err.message)) showVynosNotification('Not enough funds, please refill the wallet', 10);
-					$(buyButton).removeClass('disabled').attr('disabled', false).html("READ MORE");
+					wallet.buy(receiver, amount, gateway, meta).then(result => {
+						if (!result) return Promise.reject('vynos locked');
+						let contentKey = $('meta[property="og:url"]').attr('content')
+						localStorage[contentKey] = result.token
+						loadContent(result.token)
+						console.log('Result: ', result)
+						buyButton.style.display = 'none'
+					}).catch(err => {
+						console.error(err)
+						if (err.message && canBeHandled(err.message)) showVynosNotification('Not enough funds, please refill the wallet', 10);
+						$(buyButton).removeClass('disabled').attr('disabled', false).html("READ MORE");
+					})
 				})
 			})
 		})
@@ -77,10 +76,10 @@ window.addEventListener('load', () => {
 	let contentKey = $('meta[property="og:url"]').attr('content')
 	loadContent(localStorage[contentKey])
 	vynos.ready().then(() => {
-		vynos.setContainerStyle({ right: 'auto', left: document.getElementById('header__logo').offsetLeft + 'px' });
+		vynos.setContainerStyle({right: 'auto', left: document.getElementById('header__logo').offsetLeft + 'px'});
 	});
 })
 
 window.addEventListener('resize', () => {
-	vynos.setContainerStyle({ right: 'auto', left: document.getElementById('header__logo').offsetLeft + 'px' });
+	vynos.setContainerStyle({right: 'auto', left: document.getElementById('header__logo').offsetLeft + 'px'});
 })
